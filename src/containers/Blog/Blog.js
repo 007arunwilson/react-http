efcb3d80fbfd19cbcc8f6efca4bafabc74c527bf
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-17 18:00:30 +0530

    Added axios package; used Get request in 'componenetDidMount' method; Updated the get request data and displayed;

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,41 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+
+    state = {
+        posts:[]
+    };
+
+    componentDidMount(){
+
+        console.log('Compoenent Did Mount executed ...');
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>{
+            console.log(response.data);
+            let resposne_data = response.data;
+            let sliced_response = resposne_data.slice(0,6);
+            let updated_posts = sliced_response.map(post=>{
+                return {...post,author:'Max Corner'};
+            })
+            this.setState({posts:updated_posts});
+
+        })
+
+    }
+
     render () {
+
+        let posts = this.state.posts.map(post=><Post 
+            title={post.title}
+            author={post.author}
+            key={post.id} 
+            />);
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {!posts.length?<span style={{color:'#ccc',margin:'20px'}} >Loading Post ...</span>:posts}
                 </section>
                 <section>
                     <FullPost />
