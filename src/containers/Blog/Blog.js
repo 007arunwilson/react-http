5bf618489c0c2178199d7c5adcb4eb4bb3f62946
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-19 18:29:22 +0530

    Folder struture restructured to support routing;

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,57 +1,12 @@
 import React, { Component } from 'react';
 import axios from '../../axiosInstance';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
 
-
-    state = {
-        posts:[],
-        postid:null,
-        error:false,
-    };
-
-    componentDidMount(){
-
-        axios.get('/posts')
-        .then(response=>{
-            let resposne_data = response.data;
-            let sliced_response = resposne_data.slice(0,6);
-            let updated_posts = sliced_response.map(post=>{
-                return {...post,author:'Max Corner'};
-            })
-            this.setState({posts:updated_posts});
-
-        })
-        .catch(error=>{
-
-            this.setState({error:true})
-
-        })
-
-    }
-
-    onPostSelectionHandler = (post_id) => {
-        
-        console.log('Post slected',post_id);
-        this.setState({postid:post_id});
-
-    }
-
-    render () {
-
-        
-
-        let posts = this.state.posts.map(post=><Post 
-            title={post.title}
-            author={post.author}
-            key={post.id}
-            clicked={()=>{this.onPostSelectionHandler(post.id)}}
-            />);
+    render() {
 
         return (
             <div className="Blog" >
@@ -63,15 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {this.state.error?<p style={{textAlign:'center',color:'red'}}  >Something went wrong!</p>:(!posts.length?<span style={{color:'#ccc',margin:'20px'}} >Loading Post ...</span>:posts)}
-                </section>
-                <section>
-                    <FullPost postid={this.state.postid} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+
+                <Posts/>
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from '../../../axiosInstance';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+
+    state = {
+        posts: [],
+    };
+
+    componentDidMount() {
+
+        axios.get('/posts')
+            .then(response => {
+                let resposne_data = response.data;
+                let sliced_response = resposne_data.slice(0, 6);
+                let updated_posts = sliced_response.map(post => {
+                    return { ...post, author: 'Max Corner' };
+                })
+                this.setState({ posts: updated_posts });
+
+            })
+            .catch(error => {
+
+                console.log(error);
+                // this.setState({ error: true })
+
+            })
+
+    }
+
+
+    onPostSelectionHandler = (post_id) => {
+
+        console.log('Post slected', post_id);
+        this.setState({ postid: post_id });
+
+    }
+
+
+    render() {
+
+        let posts = this.state.posts.map(post => <Post
+            title={post.title}
+            author={post.author}
+            key={post.id}
+            clicked={() => { this.onPostSelectionHandler(post.id) }}
+        />);
+
+        return (
+            <section className="Posts">
+                {this.state.error ? <p style={{ textAlign: 'center', color: 'red' }}  >Something went wrong!</p> : (!posts.length ? <span style={{ color: '#ccc', margin: '20px' }} >Loading Post ...</span> : posts)}
+            </section>
+        );
+
+
+    }
+
+
+}
+
+export default Posts;
