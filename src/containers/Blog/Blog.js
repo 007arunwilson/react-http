d175f0836c0434d018c7048e101709b05ae28e94
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-20 11:26:10 +0530

    New Route 'about-post' added direct render method  and  Link about-post added;

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from '../../axiosInstance';
-import {Route,Link} from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import Newpost from './NewPost/NewPost';
@@ -17,19 +17,33 @@ class Blog extends Component {
                         <ul>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to={{
-                                pathname:'/new-post',
-                                hash:'#_hash',
-                                search:'?q_term=lorem'
+                                pathname: '/new-post',
+                                hash: '#_hash',
+                                search: '?q_term=lorem'
                             }}>New post</Link></li>
+                            <li><Link to={{
+                                pathname:"/about-blog",
+                                hash:"#somehash"
+                            }}>About post</Link>
+                            </li>
                         </ul>
                     </nav>
                 </header>
-                
+
                 {/* <Route path="/" exact render={()=><h1>Home base path</h1> } />
                 <Route path="/" render={()=><h1>Home base 2nd one</h1> } /> */}
 
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={Newpost} />
+                <Route path="/about-blog" render={() => {
+
+                    return (<Fragment>
+                        <div style={{ textAlign: 'center' }}>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
+                    </Fragment>);
+
+                }} />
 
             </div>
         );
