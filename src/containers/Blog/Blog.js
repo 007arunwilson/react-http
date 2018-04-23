cff600f001014b9571d0da366e175d6cf17537ae
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-23 16:19:37 +0530

    Added 'asyncComponent' for async compoenent loading;

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,13 +3,22 @@ import axios from '../../axiosInstance';
 import { Route, NavLink,Switch,Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import Newpost from './NewPost/NewPost';
+//import Newpost from './NewPost/NewPost';
 import Aboutblog from './AboutBlog/AboutBlog';
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
+
+
+const AsyncNewPost = asyncComponent(()=>{
+
+    console.log('Importing New Post .....');
+    return import('../../containers/Blog/NewPost/NewPost');
+
+})
 
 class Blog extends Component {
 
-    state = {auth:false};
+    state = {auth:true};
 
     render() {
 
@@ -43,14 +52,17 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=><h1>Home base path</h1> } />
                 <Route path="/" render={()=><h1>Home base 2nd one</h1> } /> */}
                 <Switch>
-                    <Route path="/new-post" render={((props)=><Newpost {...props} auth={this.state.auth} />)}/>
+                    <Route path="/new-post" render={((props)=><AsyncNewPost {...props} auth={this.state.auth} />)}/>
                     <Route path="/about-blog" exact component={Aboutblog} />
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Redirect from="/" exact to="/posts" />
+                    <Route render={()=>(
+                        <p style={{color:'red',textAlign:'center',fontSize:'26px'}} >Oops, we lost somewhere together ...</p>
+                    )} />
                     {/* <Route path="/" exact component={Posts} /> */}
                 </Switch>
 
-                <button onClick={()=>{ this.setState({'auth':true}); }} >Set Auth</button>
+                {/* <button onClick={()=>{ this.setState({'auth':true}); }} >Set Auth</button> */}
 
             </div>
         );
